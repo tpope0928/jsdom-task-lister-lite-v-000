96b2a0b55601d5c4faf3802c30b688c9076dba52
Thomas Pope <tpope0928@gmail.com>
2020-03-29 21:52:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDes = document.getElementById("new-task-description");
+
+  const newTaskList = document.getElementById("tasks");
+
+
 });
